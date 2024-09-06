e8215c7dcddd855a700ffeed7fbca9549a99a43c
tarassych <taras.sych@gmail.com>
2024-09-06 14:14:23 +0300

    test commit

--- vapiapp/config/env.config.ts
+++ vapiapp/config/env.config.ts
@@ -1,8 +1,8 @@
 export const envConfig = {
   vapi: {
-    apiUrl: process.env.NEXT_PUBLIC_VAPI_API_URL ?? "https://api.vapi.ai",
-    token: process.env.NEXT_PUBLIC_VAPI_WEB_TOKEN ?? "9cd9a527-acad-4ad5-beaf-9b5d49e1f002", 
-    assistantId: process.env.NEXT_PUBLIC_VAPI_ASSISTANT_ID ?? 'fcc7129d-2556-4f33-9490-6c293332f213'
+    apiUrl: process.env.NEXT_PUBLIC_VAPI_API_URL ?? "https://api.vapi.ai", //
+    token: process.env.NEXT_PUBLIC_VAPI_WEB_TOKEN ?? "9cd9a527-acad-4ad5-beaf-9b5d49e1f002", //
+    assistantId: process.env.NEXT_PUBLIC_VAPI_ASSISTANT_ID ?? 'fcc7129d-2556-4f33-9490-6c293332f213' //
   },
 };
 
