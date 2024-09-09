dfa2744defe4c474ececfaab43147f96f0662eae
tarassych <taras.sych@gmail.com>
2024-09-09 23:49:24 +0300

    changed url structure

--- hosting/files/_next/static/chunks/pages/index-52b561bb26aa8d8d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{8312:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return t(6209)}])},6209:function(n,u,t){"use strict";function _(){return null}t.r(u),t.d(u,{default:function(){return _}})}},function(n){n.O(0,[888,774,179],function(){return n(n.s=8312)}),_N_E=n.O()}]);
