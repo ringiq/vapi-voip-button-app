f6442658dea18153b1d59a7e454d807c6a6ef490
tarassych <taras.sych@gmail.com>
2025-02-06 00:32:22 +0200

    rebuild redeploy

--- next.config.mjs
+++ next.config.mjs
@@ -2,30 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  distDir: 'standalone/_next', // Moves _next into standalone/_next
-
-  async rewrites() {
-    return [
-      {
-        source: "/_next/:path*", // Redirect _next requests
-        destination: "/standalone/_next/:path*",
-      },
-    ];
-  },
-
-  async headers() {
-    return [
-      {
-        source: "/_next/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
